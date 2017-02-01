30c2403c02ac4e15b81702b7274fbcc6f377d617
Sahil Motwani <sahil.motwani91@gmail.com>
2017-02-01 00:38:18 -0500

    Update RSA.java

--- RSA.java
+++ RSA.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 /**
  * @author sahil
  * 
- * 	Step 1: Generate two large prime numbers p and q.
+ *  Step 1: Generate two large prime numbers p and q.
 	Step 2: Calculate n = p*q
 	Step 3: Calculate Euler's totient function ø(n) = (p – 1)*(q – 1)
 	Step 4: Find e such that it is relatively prime with ø(n) that is the gcd(e, ø(n)) = 1
@@ -15,9 +15,8 @@ import java.io.IOException;
 
 	Public Key is (n, e) and Private Key is (d, n).
 
-	Encryption - Cybertext = (PlainText ^ e) mod n
-	Decryption - PlainText = (CyberText ^ d) mod n
-
+	Encryption - Ciphertext = (PlainText ^ e) mod n
+	Decryption - PlainText = (Ciphertext ^ d) mod n
  *
  */
 
@@ -43,24 +42,24 @@ public class RSA
 
 		/* Step 2: Calculate n = p.q */
 		n = p.multiply(q);
-		
+
 		printPrimes(p,q,n);
-		
 		// Step 3. Calculate ø(n) = (p-1)*(q-1)
 		phiN = p.subtract(BigInteger.valueOf(1));
-		phiN = phiN.multiply( q.subtract( BigInteger.valueOf(1)));
+		phiN = phiN.multiply( q.subtract( BigInteger.ONE));
 
 		//Step 4 . Find e such that gcd(e ,ø(n)) = 1 and e < ø(n)
 		//this while loop will run until gcd of e and ø(n) is not 1
 
 		switch(choice){
 		case 1:
-		e = new BigInteger(2*bitLength, new Random());
-		while( (e.compareTo(phiN) != 1) || (e.gcd(phiN).compareTo(BigInteger.valueOf(1)) != 0))
-		{
 			e = new BigInteger(2*bitLength, new Random());
-		}
-		break;
+			while( (e.compareTo(phiN) != 1) || (e.gcd(phiN).compareTo(BigInteger.ONE) != 0))
+			{
+				e = new BigInteger(2*bitLength, new Random());
+			}
+
+			break;
 		case 2:
 			e= 	getPublicKey();
 			break;
@@ -87,12 +86,11 @@ public class RSA
 			System.out.println("Enter a large prime number");
 			e =scan.nextBigInteger();
 		}
-		while( (e.compareTo(phiN) != 1) || (e.gcd(phiN).compareTo(BigInteger.valueOf(1)) != 0));
-		
+		while( (e.compareTo(phiN) != 1) || (e.gcd(phiN).compareTo(BigInteger.ONE) != 0));
 		return e;
 	}
-	
-	
+
+
 	public BigInteger encrypt(BigInteger plaintext)
 	{
 		return plaintext.modPow(e, n);
@@ -114,13 +112,12 @@ public class RSA
 		System.out.println("Press 1 to automatically generate public key or press 2 to enter your own public key");
 		System.out.println("If entering your own key, make it significantly larger for eg. 1280367512230007307189");
 		int choice =scan.nextInt();
-		
 		if(choice != 1 && choice != 2){
 			System.out.println("Invalid argument.");
 			System.exit(0);
 		}
 		RSA rsaTest = new RSA(choice);
-		
+
 		BigInteger bigPlainText, bigCipherText;
 
 		//Generate byte array from string and create big integer for it
@@ -138,5 +135,6 @@ public class RSA
 
 		//recover the original string from the big integer value and print
 		System.out.println("After Decryption Plaintext : " +  new String(bigPlainText.toByteArray()));
+		scan.close();
 	}
 }
